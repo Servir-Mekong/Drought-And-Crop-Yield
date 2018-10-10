9d3e52887e8a777d5d77d5bfca009c1da78ee894
unknown <bionicbiplov45@gmail.com>
2018-10-10 10:51:08 +0700

    base layer control

--- modules/map/client/controllers/map.client.controller.js
+++ modules/map/client/controllers/map.client.controller.js
@@ -469,6 +469,25 @@
 			zoomControl: false
 		});
 
+		// Base Layers
+        var Esri_WorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, ' +
+            'Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+        });
+
+        var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+            attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+		});
+
+        var baseLayers = {
+			'Grayscale': basemap_mapbox,
+            'Satellite Imagery': Esri_WorldImagery,
+            'Topo Map': Esri_WorldTopoMap
+        };
+
+        // Control
+        L.control.layers(baseLayers, null, { position: 'bottomright' }).addTo(map);
+
 		// Marker Clusters
 		var markerCluster = L.markerClusterGroup();
 
